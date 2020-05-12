d8d95b3e30adda51ba9be5f5c449f396c59b563c
novan921 <anakinskywalk1@gmail.com>
2020-05-12 11:41:21 +0700

    Update ch_get.php

--- project_collocare/json/ch_get.php
+++ project_collocare/json/ch_get.php
@@ -2,10 +2,15 @@
     $chapter = $_GET["ch"];
     $total_items  = count(glob("../chapters/*", GLOB_ONLYDIR));
     $result->NUMBER_OF_CHAPTERS = $total_items;
-    $result->chapter = $chapter;
+    // $result->chapter = $chapter;
+    
     $images = array();
-    foreach(glob("../chapters/chapter".$chapter."/*.*") as $file) {
-        array_push($images, "http://collocare.herokuapp.com/project_collocare/chapters/chapter".$chapter."/".basename($file));
+    // foreach(glob("../chapters/chapter".$chapter."/*.*") as $file) {
+    //     array_push($images, "http://collocare.herokuapp.com/project_collocare/chapters/chapter".$chapter."/".basename($file));
+    // }
+
+    foreach(glob("../chapters/chapter/*.*") as $file) {
+        array_push($images, $file);
     }
     sort($images,SORT_NATURAL);
     // $sorted_images = array();
