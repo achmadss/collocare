8475ebc5be573bc7c36e620ef45f9308479579a8
novan921 <anakinskywalk1@gmail.com>
2020-05-16 09:44:18 +0700

    up

--- project_collocare/json/ch_count.php
+++ project_collocare/json/ch_count.php
@@ -1,8 +1,22 @@
 <?php
     error_reporting(1);
     // $title = $_GET["title"];
-    if(isset($_FILES["inputAssets"])) {
-        echo "ADA COK FILENYA";
+    if (isset($_FILES['inputAssets'])) {
+        $myFile = $_FILES['inputAssets'];
+        $fileCount = count($myFile["name"]);
+
+        for ($i = 0; $i < $fileCount; $i++) {
+            ?>
+                <p>File #<?= $i+1 ?>:</p>
+                <p>
+                    Name: <?= $myFile["name"][$i] ?><br>
+                    Temporary file: <?= $myFile["tmp_name"][$i] ?><br>
+                    Type: <?= $myFile["type"][$i] ?><br>
+                    Size: <?= $myFile["size"][$i] ?><br>
+                    Error: <?= $myFile["error"][$i] ?><br>
+                </p>
+            <?php
+        }
     }
 
     // if(count($_FILES['uploads']['inputAssets'])) {
