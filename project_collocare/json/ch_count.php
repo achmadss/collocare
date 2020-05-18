901ea4aca132074a6881e14ab48d4b1daba8361f
novan921 <anakinskywalk1@gmail.com>
2020-05-18 10:54:23 +0700

    up

--- project_collocare/json/ch_count.php
+++ project_collocare/json/ch_count.php
@@ -5,18 +5,18 @@
         $myFile = $_FILES['inputAssets'];
         $fileCount = count($myFile["name"]);
         mkdir($title+"");
-        // for ($i = 0; $i < $fileCount; $i++) {
-        //     ?>
-        //         <p>File #<?= $i+1 ?>:</p>
-        //         <p>
-        //             Name: <?= $myFile["name"][$i] ?><br>
-        //             Temporary file: <?= $myFile["tmp_name"][$i] ?><br>
-        //             Type: <?= $myFile["type"][$i] ?><br>
-        //             Size: <?= $myFile["size"][$i] ?><br>
-        //             Error: <?= $myFile["error"][$i] ?><br>
-        //         </p>
-        //     <?php
-        // }
+        for ($i = 0; $i < $fileCount; $i++) {
+            ?>
+                <p>File #<?= $i+1 ?>:</p>
+                <p>
+                    Name: <?= $myFile["name"][$i] ?><br>
+                    Temporary file: <?= $myFile["tmp_name"][$i] ?><br>
+                    Type: <?= $myFile["type"][$i] ?><br>
+                    Size: <?= $myFile["size"][$i] ?><br>
+                    Error: <?= $myFile["error"][$i] ?><br>
+                </p>
+            <?php
+        }
     }
 
     // if(count($_FILES['uploads']['inputAssets'])) {
