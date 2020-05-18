812051120f3332c9f80b1ea83282fde755ed89a9
novan921 <anakinskywalk1@gmail.com>
2020-05-18 11:09:15 +0700

    up

--- project_collocare/json/ch_count.php
+++ project_collocare/json/ch_count.php
@@ -1,12 +1,11 @@
 <?php
-    error_reporting(1);
     $title = $_POST["title"];
     if (isset($_FILES['inputAssets'])) {
         $myFile = $_FILES['inputAssets'];
         $fileCount = count($myFile["name"]);
         mkdir("project_collocare/chapters/"+$title, 0700);
     }
-
+    print_r(error_get_last());
     // if(count($_FILES['uploads']['inputAssets'])) {
     //     foreach ($_FILES['uploads']['inputAssets'] as $file) {
             
